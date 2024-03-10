770d2c51931dff6edeed29aee2e6982a8c4c7f84
Kavya Sonkar <kavyasonkar10@gmail.com>
2024-03-10 17:18:41 +0100

    next privous button done and optimized#28

--- src/components/NewsItems.js
+++ src/components/NewsItems.js
@@ -6,11 +6,11 @@ export class NewsItems extends Component {
     return (
       <div className='my-3'>
         <div className="card" style={{width: "18rem"}}>
-  <img src={imageUrl} className="card-img-top" alt="..."/>
+  <img src={!imageUrl?"https://www.hindustantimes.com/ht-img/img/2024/03/07/1600x900/ANI-20240304139-0_1709806713113_1709806795091.jpg":imageUrl} className="card-img-top" alt="..."/>
   <div className="card-body">
     <h5 className="card-title">{title}...</h5>
     <p className="card-text">{description}...</p>
-    <a href={url} target="_blank" className="btn btn-sm btn-primary">Read More</a>
+    <a href={url} target="_blank" className="btn btn-sm btn-dark">Read More</a>
   </div>
 </div>
       </div>

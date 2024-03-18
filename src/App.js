1152c5469ea5a2b3a6d203e100073d606fa763d7
Kavya Sonkar <kavyasonkar10@gmail.com>
2024-03-18 13:33:37 +0100

    31 completed

--- src/App.js
+++ src/App.js
@@ -22,12 +22,12 @@ export default class App extends Component {
 
         <Route exact path="/" element={<News key='general'pageSize={12} country='in' category='general'/>}></Route>
 
-        <Route path='/business' element={<News key='business' pageSize={12} country='us' category='business' />}></Route>
-            <Route path='/entertainment' element={<News key='entertainment' pageSize={12} country='us' category='entertainment' />}></Route>
-            <Route path='/health' element={<News key='health' pageSize={12} country='us' category='health' />}></Route>
-            <Route path='/science' element={<News key='science' pageSize={12} country='us' category='science' />}></Route>
-            <Route path='/sports' element={<News key='sports' pageSize={12} country='us' category='sports' />}></Route>
-            <Route path='/technology' element={<News key='technology' pageSize={12} country='us' category='technology' />}></Route>
+        <Route path='/business' element={<News key='business' pageSize={12} country='in' category='business' />}></Route>
+            <Route path='/entertainment' element={<News key='entertainment' pageSize={12} country='in' category='entertainment' />}></Route>
+            <Route path='/health' element={<News key='health' pageSize={12} country='in' category='health' />}></Route>
+            <Route path='/science' element={<News key='science' pageSize={12} country='in' category='science' />}></Route>
+            <Route path='/sports' element={<News key='sports' pageSize={12} country='in' category='sports' />}></Route>
+            <Route path='/technology' element={<News key='technology' pageSize={12} country='in' category='technology' />}></Route>
 
         
         </Routes>
